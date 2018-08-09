1a5c5ee7c0bcecace4c22231068f72801f670be5
brandonjunus <brandonjunus2@gmail.com>
2018-08-09 15:05:39 -0700

    made progress on a solution for solver portion

--- src/solvers.js
+++ src/solvers.js
@@ -13,13 +13,65 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+window.counterObjectIncrement = function(counterObject, n){
+  // debugger;
+  counterObject[0] ++;
+  for(let key = 0; key < n; key++){
+    if (counterObject[n - 1] > n){
+      console.log("We're Done");
+      return
+    } else if (counterObject[key] > n){
+      counterObject[key] = 0;
+      counterObject[key + 1]++;
+    }
+  }
+  console.log(counterObject)
+  return(counterObject);
+} 
+
+window.makeMatrixFromCounterObject = function(counterObject, n){
+  var matrix = [];
+  for (let key in counterObject){
+    var currentRow = [];
+    for(let i = 0; i < n; i++){
+      if(counterObject[key] === i){
+        currentRow.push(1);
+      }  else {
+        currentRow.push(0)
+      }
+    }
+    matrix.push(currentRow);
+  }
+  return matrix
+}
+
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var listOfSatisfactoryMatrices = [];
+  var counterObject = {};
+  for (var i = 0; i < n; i++) {
+    counterObject[i] = 0;
+  }
+  var test = true;
+  // debugger;
+  // var x = 0
+  while (test === true){
+    
+    var currentMatrix = makeMatrixFromCounterObject(counterObject, n);
+    var currentBoard = new Board (currentMatrix);
+    if(!currentBoard.hasAnyRooksConflicts()){
+      listOfSatisfactoryMatrices = currentMatrix;
+      // listOfSatisfactoryMatrices.push(solution);
+      test = false;
+    } else{
+      counterObjectIncrement(counterObject, n);
+    }
+    // x++;
+  }
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return listOfSatisfactoryMatrices;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
